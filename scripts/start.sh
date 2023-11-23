1d7a22da94d424d1265abec250ce1757cd2efb1e
JoonYoung2 <90246124+JoonYoung2@users.noreply.github.com>
2023-11-24 06:37:13 +0900

    scripts update

--- scripts/start.sh
+++ scripts/start.sh
@@ -2,6 +2,7 @@
 
 PROJECT_ROOT="/home/ubuntu/app"
 WAR_FILE="$PROJECT_ROOT/spring-webapp.war"
+WAR_FILE2="$PROJECT_ROOT/spring-webapp2.war"
 
 APP_LOG="$PROJECT_ROOT/application.log"
 ERROR_LOG="$PROJECT_ROOT/error.log"
@@ -11,11 +12,17 @@ TIME_NOW=$(date +%c)
 
 # build 파일 복사
 echo "$TIME_NOW > $WAR_FILE 파일 복사" >> $DEPLOY_LOG
-cp $PROJECT_ROOT/build/libs/*.war $WAR_FILE
+echo "$TIME_NOW > $WAR_FILE2 파일 복사" >> $DEPLOY_LOG
+cp $PROJECT_ROOT/project/build/libs/*.war $WAR_FILE
+cp $PROJECT_ROOT/restApi/api/build/libs/*.war $WAR_FILE2
 
 # jar 파일 실행
 echo "$TIME_NOW > $WAR_FILE 파일 실행" >> $DEPLOY_LOG
+echo "$TIME_NOW > $WAR_FILE2 파일 실행" >> $DEPLOY_LOG
 nohup java -jar $WAR_FILE > $APP_LOG 2> $ERROR_LOG &
+nohup java -jar $WAR_FILE2 > $APP_LOG 2> $ERROR_LOG &
 
 CURRENT_PID=$(pgrep -f $WAR_FILE)
+CURRENT_PID2=$(pgrep -f $WAR_FILE2)
 echo "$TIME_NOW > 실행된 프로세스 아이디 $CURRENT_PID 입니다." >> $DEPLOY_LOG
+echo "$TIME_NOW > 실행된 프로세스 아이디 $CURRENT_PID2 입니다." >> $DEPLOY_LOG
--- scripts/stop.sh
+++ scripts/stop.sh
@@ -2,6 +2,7 @@
 
 PROJECT_ROOT="/home/ubuntu/app"
 WAR_FILE="$PROJECT_ROOT/spring-webapp.war"
+WAR_FILE2="$PROJECT_ROOT/spring-webapp2.war"
 
 DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
 
@@ -9,11 +10,13 @@ TIME_NOW=$(date +%c)
 
 # 현재 구동 중인 애플리케이션 pid 확인
 CURRENT_PID=$(pgrep -f $WAR_FILE)
+CURRENT_PID2=$(pgrep -f $WAR_FILE2)
 
 # 프로세스가 켜져 있으면 종료
 if [ -z $CURRENT_PID ]; then
   echo "$TIME_NOW > 현재 실행중인 애플리케이션이 없습니다" >> $DEPLOY_LOG
 else
   echo "$TIME_NOW > 실행중인 $CURRENT_PID 애플리케이션 종료 " >> $DEPLOY_LOG
+  echo "$TIME_NOW > 실행중인 $CURRENT_PID2 애플리케이션 종료 " >> $DEPLOY_LOG
   kill -15 $CURRENT_PID
 fi
