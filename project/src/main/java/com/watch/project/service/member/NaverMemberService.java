c57695b21881364c0158b656f90bc98eafae5cc3
Sua90827 <118704802+Sua90827@users.noreply.github.com>
2023-11-27 21:38:57 +0900

    working on adding joinedDate and profileImg

--- project/src/main/java/com/watch/project/service/member/NaverMemberService.java
+++ project/src/main/java/com/watch/project/service/member/NaverMemberService.java
@@ -158,6 +158,8 @@ public class NaverMemberService {
     	user.setSocialLoginId(res_obj.get("id").toString().replaceAll("\"", ""));        	
 
     	user.setAccessToken(str_result);
+    	
+//    	user.setJoinedDate(); 여기부터 하자아아아아아아아아아아아아ㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏ
     	int storageResult = saveMemberInfo(user); //회원 정보 저장
 //    	int updateResult = repo.updateNaverAgreement(email);
     	
